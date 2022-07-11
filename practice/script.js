21cd4c710d479f1414c9e4bb6300c9d8a9df23bb
Abdullahiabdulkadir <Abdullahiabdulkadir0213999@gmail.com>
2022-07-11 22:47:43 +0100

    booleans.js

---
+++ practice/script.js
@@ -0,0 +1,13 @@
+let name = prompt("Enter name")
+
+if(name.toLowerCase() == "asad" ){
+  let isCat = true;
+  alert("CORRECT")
+  document.getElementById("demo2").innerHTML = "correct";
+}else {
+  let = isCat = false
+  alert("INCORRECT")
+  document.getElementById("demo2").innerHTML = "INCORRECT"
+}
+  
+
